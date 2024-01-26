8075b2746e01c34180b42688d9eebf84d1328ebb
Natali <Natali@ANTONS_XP>
2024-01-26 10:50:21 +0100

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace TestMailKit
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnRead = new Button();
+            tbIMAPServer = new TextBox();
+            lblIMAPServer = new Label();
+            lblIMAPPort = new Label();
+            tbIMAPPort = new TextBox();
+            lblUsername = new Label();
+            tbUsername = new TextBox();
+            lblPassword = new Label();
+            tbPassword = new TextBox();
+            tbSubject = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            tbDate = new TextBox();
+            btnClear = new Button();
+            btnReadAndDelete = new Button();
+            lblFrom = new Label();
+            tbFrom = new TextBox();
+            tbContent = new TextBox();
+            lblContent = new Label();
+            SuspendLayout();
+            // 
+            // btnRead
+            // 
+            btnRead.Location = new Point(80, 413);
+            btnRead.Name = "btnRead";
+            btnRead.Size = new Size(679, 58);
+            btnRead.TabIndex = 0;
+            btnRead.Text = "Read and move to trash";
+            btnRead.UseVisualStyleBackColor = true;
+            btnRead.Click += btnRead_ClickAsync;
+            // 
+            // tbIMAPServer
+            // 
+            tbIMAPServer.Location = new Point(313, 79);
+            tbIMAPServer.Name = "tbIMAPServer";
+            tbIMAPServer.Size = new Size(442, 47);
+            tbIMAPServer.TabIndex = 1;
+            tbIMAPServer.Text = "imap.yandex.ru";
+            // 
+            // lblIMAPServer
+            // 
+            lblIMAPServer.AutoSize = true;
+            lblIMAPServer.Location = new Point(80, 79);
+            lblIMAPServer.Name = "lblIMAPServer";
+            lblIMAPServer.Size = new Size(186, 41);
+            lblIMAPServer.TabIndex = 2;
+            lblIMAPServer.Text = "IMAP Server:";
+            lblIMAPServer.TextAlign = ContentAlignment.TopRight;
+            // 
+            // lblIMAPPort
+            // 
+            lblIMAPPort.AutoSize = true;
+            lblIMAPPort.Location = new Point(80, 154);
+            lblIMAPPort.Name = "lblIMAPPort";
+            lblIMAPPort.Size = new Size(79, 41);
+            lblIMAPPort.TabIndex = 3;
+            lblIMAPPort.Text = "Port:";
+            // 
+            // tbIMAPPort
+            // 
+            tbIMAPPort.Location = new Point(313, 151);
+            tbIMAPPort.MaxLength = 6;
+            tbIMAPPort.Name = "tbIMAPPort";
+            tbIMAPPort.Size = new Size(442, 47);
+            tbIMAPPort.TabIndex = 4;
+            tbIMAPPort.Text = "993";
+            tbIMAPPort.KeyPress += tbIMAPPort_KeyPress;
+            // 
+            // lblUsername
+            // 
+            lblUsername.AutoSize = true;
+            lblUsername.Location = new Point(80, 250);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(159, 41);
+            lblUsername.TabIndex = 5;
+            lblUsername.Text = "Username:";
+            // 
+            // tbUsername
+            // 
+            tbUsername.Location = new Point(313, 247);
+            tbUsername.Name = "tbUsername";
+            tbUsername.Size = new Size(442, 47);
+            tbUsername.TabIndex = 6;
+            tbUsername.Text = "senefishka";
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Location = new Point(80, 331);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(150, 41);
+            lblPassword.TabIndex = 7;
+            lblPassword.Text = "Password:";
+            // 
+            // tbPassword
+            // 
+            tbPassword.Location = new Point(311, 325);
+            tbPassword.Name = "tbPassword";
+            tbPassword.Size = new Size(448, 47);
+            tbPassword.TabIndex = 8;
+            tbPassword.Text = "Fishka1234";
+            // 
+            // tbSubject
+            // 
+            tbSubject.Location = new Point(1278, 154);
+            tbSubject.Name = "tbSubject";
+            tbSubject.Size = new Size(896, 47);
+            tbSubject.TabIndex = 9;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(1092, 151);
+            label1.Name = "label1";
+            label1.Size = new Size(123, 41);
+            label1.TabIndex = 10;
+            label1.Text = "Subject:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(1128, 253);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 41);
+            label2.TabIndex = 11;
+            label2.Text = "Date:";
+            // 
+            // tbDate
+            // 
+            tbDate.Location = new Point(1278, 253);
+            tbDate.Name = "tbDate";
+            tbDate.Size = new Size(893, 47);
+            tbDate.TabIndex = 12;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(1896, 524);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(275, 58);
+            btnClear.TabIndex = 13;
+            btnClear.Text = "Clear mail data";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // btnReadAndDelete
+            // 
+            btnReadAndDelete.Location = new Point(80, 524);
+            btnReadAndDelete.Name = "btnReadAndDelete";
+            btnReadAndDelete.Size = new Size(679, 58);
+            btnReadAndDelete.TabIndex = 14;
+            btnReadAndDelete.Text = "Read and delete";
+            btnReadAndDelete.UseVisualStyleBackColor = true;
+            btnReadAndDelete.Click += btnReadAndDelete_Click;
+            // 
+            // lblFrom
+            // 
+            lblFrom.AutoSize = true;
+            lblFrom.Location = new Point(1118, 65);
+            lblFrom.Name = "lblFrom";
+            lblFrom.Size = new Size(94, 41);
+            lblFrom.TabIndex = 15;
+            lblFrom.Text = "From:";
+            // 
+            // tbFrom
+            // 
+            tbFrom.Location = new Point(1278, 62);
+            tbFrom.Name = "tbFrom";
+            tbFrom.Size = new Size(896, 47);
+            tbFrom.TabIndex = 16;
+            // 
+            // tbContent
+            // 
+            tbContent.Location = new Point(1278, 354);
+            tbContent.Multiline = true;
+            tbContent.Name = "tbContent";
+            tbContent.Size = new Size(896, 118);
+            tbContent.TabIndex = 17;
+            // 
+            // lblContent
+            // 
+            lblContent.AutoSize = true;
+            lblContent.Location = new Point(1092, 354);
+            lblContent.Name = "lblContent";
+            lblContent.Size = new Size(132, 41);
+            lblContent.TabIndex = 18;
+            lblContent.Text = "Content:";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(2261, 799);
+            Controls.Add(lblContent);
+            Controls.Add(tbContent);
+            Controls.Add(tbFrom);
+            Controls.Add(lblFrom);
+            Controls.Add(btnReadAndDelete);
+            Controls.Add(btnClear);
+            Controls.Add(tbDate);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(tbSubject);
+            Controls.Add(tbPassword);
+            Controls.Add(lblPassword);
+            Controls.Add(tbUsername);
+            Controls.Add(lblUsername);
+            Controls.Add(tbIMAPPort);
+            Controls.Add(lblIMAPPort);
+            Controls.Add(lblIMAPServer);
+            Controls.Add(tbIMAPServer);
+            Controls.Add(btnRead);
+            Name = "MainForm";
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnRead;
+        private TextBox tbIMAPServer;
+        private Label lblIMAPServer;
+        private Label lblIMAPPort;
+        private TextBox tbIMAPPort;
+        private Label lblUsername;
+        private TextBox tbUsername;
+        private Label lblPassword;
+        private TextBox tbPassword;
+        private TextBox tbSubject;
+        private Label label1;
+        private Label label2;
+        private TextBox tbDate;
+        private Button btnClear;
+        private Button btnReadAndDelete;
+        private Label lblFrom;
+        private TextBox tbFrom;
+        private TextBox tbContent;
+        private Label lblContent;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MailKit;
+using MailKit.Net;
+using MailKit.Net.Imap;
+using MimeKit;
+using Org.BouncyCastle.Asn1.X509;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+
+namespace TestMailKit
+{
+    public partial class MainForm : Form
+    {
+        public string EXT_IMAP_SERVER = "imap.yandex.ru";
+        public int EXT_IMAP_PORT = 993;
+        public string EXT_USERNAME = "";
+        public string EXT_PASSWORD = "";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnRead_ClickAsync(object sender, EventArgs e)
+        {
+            ClearMailDate();
+            GetAuthorizationData();           
+            await ReadAndMoveToTrashMessageAsync();
+        }
+
+        private async void btnReadAndDelete_Click(object sender, EventArgs e)
+        {
+            ClearMailDate();
+            GetAuthorizationData();
+            await ReadAndDeleteMessageAsync();
+        }
+
+
+
+        private async Task ReadAndMoveToTrashMessageAsync()
+        {
+            using (var client = new ImapClient())
+            {
+                client.Connect(EXT_IMAP_SERVER, EXT_IMAP_PORT, true);
+                client.Authenticate(EXT_USERNAME, EXT_PASSWORD);
+                                     
+                var inbox = client.Inbox;                
+                inbox.Open(FolderAccess.ReadWrite);
+                var trash = client.GetFolder(SpecialFolder.Trash);
+                var lastUnreadIndex = inbox.FirstUnread;
+                var lastMessage = client.Inbox.GetMessage(lastUnreadIndex);
+
+                await inbox.AddFlagsAsync(lastUnreadIndex, MessageFlags.Seen, true);
+                await inbox.MoveToAsync(lastUnreadIndex, trash);
+                FillMailData(lastMessage);
+
+                client.Disconnect(true);
+
+            }
+        }
+
+        private async Task ReadAndDeleteMessageAsync()
+        {
+            using (var client = new ImapClient())
+            {
+                client.Connect(EXT_IMAP_SERVER, EXT_IMAP_PORT, true);
+                client.Authenticate(EXT_USERNAME, EXT_PASSWORD);
+
+                var inbox = client.Inbox;
+                inbox.Open(FolderAccess.ReadWrite);
+                var lastUnreadIndex = inbox.FirstUnread;
+                var lastMessage = client.Inbox.GetMessage(lastUnreadIndex);
+
+                await inbox.AddFlagsAsync(lastUnreadIndex, MessageFlags.Deleted, true);
+                FillMailData(lastMessage);
+                client.Disconnect(true);
+            }
+        }
+       
+
+        private void tbIMAPPort_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearMailDate();
+        }
+
+        private void ClearMailDate()
+        {
+            tbFrom.Clear();
+            tbSubject.Clear();
+            tbDate.Clear();
+            tbContent.Clear();
+        }
+
+        private void GetAuthorizationData()
+        {
+            EXT_IMAP_SERVER = tbIMAPServer.Text;
+            int.TryParse(tbIMAPPort.Text, out EXT_IMAP_PORT);
+            EXT_USERNAME = tbUsername.Text;
+            EXT_PASSWORD = tbPassword.Text;
+        }
+
+        private void FillMailData(MimeMessage msg)
+        {
+            tbFrom.Text = msg.From.ToString();
+            tbSubject.Text = msg.Subject;
+            tbDate.Text = msg.Date.Date.ToLongDateString();
+            tbContent.Text = msg.TextBody ?? msg.HtmlBody;
+        }
+    }
+}
